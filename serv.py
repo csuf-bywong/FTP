c0c84907ae679a207fff30f7ffc6461f93fe64d0
csuf-bywong <bywong@csu.fullerton.edu>
2018-05-18 23:55:15 -0700

    Update serv.py

--- serv.py
+++ serv.py
@@ -61,42 +61,43 @@ while 1:
             connectionSocket.send("0")
             break
 
-        elif command == "ls":
-            ephPort = int(connectionSocket.recv(10))
-            print("Received ephemeral port: " , ephPort)
+        elif cmd == "ls":
+			print("received " , cmd)
+			ephPort = connectionSocket.recv(10)
+			print("Received ephemeral port: " , int(ephPort))
 
             #create connection for data transfer
-            servDataSock = socket(AF_INET, SOCK_STREAM)
-            servDataSock.connect(("localhost", ephPort))
+			servDataSock = socket(AF_INET, SOCK_STREAM)
+			servDataSock.connect(("localhost", ephPort))
 
             #get path
-            path = os.path.dirname(os.path.realpath(__file__))
+			path = os.path.dirname(os.path.realpath(__file__))
 
             #get dir file lis
-            ls = os.listdir(path)
+			ls = os.listdir(path)
 
             #var to store the list
-            lst = ""
-            for value in ls:
-                lst = lst + value + '\n'
+			lst = ""
+			for value in ls:
+				lst = lst + value + '\n'
 
             #get size of data
-            size = str(len(lst))
+			size = str(len(lst))
 
             #convert to 10 byte header
-            while len(size) < 10:
-                size = "0" + size
+			while len(size) < 10:
+				size = "0" + size
 
             #attach size to beginning of data
-            lst = size + lst
+			lst = size + lst
 
-            bytesSent = 0
+			bytesSent = 0
 
             #send the data
-            while len(lst) > bytesSent:
-                bytesSent += servDataSock.send(lst[bytesSent:])
+			while len(lst) > bytesSent:
+				bytesSent += servDataSock.send(lst[bytesSent:])
 
             #close connection
-            servDataSock.close()
+			servDataSock.close()
             #say server is ready for another command
-            connectionSocket.send("1")
+			connectionSocket.send("1")
